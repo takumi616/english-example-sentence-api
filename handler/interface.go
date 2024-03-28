4b407ad59423d7c7d51f77325532cee731ba03b2
takumi616 <ohohyeah321@icloud.com>
2024-03-29 00:22:50 +0900

    Add a http handler fetching sentence list.

--- handler/interface.go
+++ handler/interface.go
@@ -1,7 +1,15 @@
 package handler
 
-import "context"
+import (
+	"context"
+
+	"github.com/takumi616/english-example-sentence-api/entity"
+)
 
 type SentenceCreater interface {
 	CreateNewSentence(ctx context.Context, vocabularies []string, body string) (int, error)
+}
+
+type SentenceListFetcher interface {
+	FetchSentenceList(ctx context.Context) ([]entity.Sentence, error)
 }
