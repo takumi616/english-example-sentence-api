b292b74d8be8ac41f9b3a5713fe8e44c7da80d7b
takumi616 <ohohyeah321@icloud.com>
2024-04-01 00:23:32 +0900

    Edit comments of service package's files.

--- service/fetch_sentence.go
+++ service/fetch_sentence.go
@@ -9,9 +9,11 @@ import (
 )
 
 type FetchSentence struct {
+	//Interface to access store package's method
 	Store SentenceSelecter
 }
 
+// Fetch all sentences
 func (f *FetchSentence) FetchSentenceList(ctx context.Context) ([]entity.Sentence, error) {
 	sentences, err := f.Store.SelectSentenceList(ctx)
 	if err != nil {
@@ -22,12 +24,15 @@ func (f *FetchSentence) FetchSentenceList(ctx context.Context) ([]entity.Sentenc
 	return sentences, nil
 }
 
+// Fetch single sentence by sentence id
 func (f *FetchSentence) FetchSingleSentence(ctx context.Context, id string) (entity.Sentence, error) {
+	//Convert string into int
 	sentenceID, err := strconv.Atoi(id)
 	if err != nil {
 		log.Printf("Error occurred in service package: %v", err)
 	}
 
+	//Call store package's method, using interface
 	sentence, err := f.Store.SelectSentenceById(ctx, sentenceID)
 	if err != nil {
 		log.Printf("Error occurred in service package: %v", err)
